e121b70633eef887980734b099a3f0577712a35c
小黑兔 <173592829@qq.com>
2019-12-25 23:47:10 +0800

    提交

---
+++ src/Rename/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Rename
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                //显示帮助信息
+                return;
+            }
+
+            var input = args.Last();
+
+            var options = args.Take(args.Length - 1).ToArray();
+            RenameCore rc = new RenameCore();
+            for (int i= 0;i < options.Length;i++)
+            {
+                var o = options[i];
+                var isfinish = false;
+                switch (o.ToLower())
+                {
+                    case "-?":
+                    case "/?":
+                        {
+                            //显示帮助信息
+                            isfinish = true;
+                            break;
+                        }
+                    case "-iu":
+                        {
+                            //首字母大写
+                            rc.InitialUpper(input);
+                            isfinish = true;
+                            break;
+                        }
+                    case "-il":
+                        {
+                            //首字母小写
+                            rc.InitialLower(input);
+                            isfinish = true;
+                            break;
+                        }
+                    case "-u":
+                        {
+                            //全大写
+                            rc.FullUpper(input);
+                            isfinish = true;
+                            break;
+                        }
+                    case "-l":
+                        {
+                            //全小写
+                            rc.FullLower(input);
+                            isfinish = true;
+                            break;
+                        }
+                    default:
+                        {
+                            throw new Exception("不支持的参数");
+                            isfinish = true;
+                            break;
+                        }
+                }
+
+                if (isfinish)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ src/Rename/RenameCore.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Diagnostics;
+
+namespace Rename
+{
+    public class RenameCore
+    {
+        /// <summary>
+        /// 文件改名
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="target"></param>
+        public void FileRename(string source, string target)
+        {
+            if (!File.Exists(source))
+            {
+                throw new Exception("文件不存在！");
+            }
+
+            Console.Write("{0} -> {1}", Path.GetFileName(source).PadRight(20), target.PadRight(20));
+            Process.Start("cmd.exe", $"/c ren {source} {target}");
+            
+            Console.WriteLine("  OK!");
+
+        }
+
+        /// <summary>
+        /// 递归处理文件夹
+        /// </summary>
+        /// <param name="dir"></param>
+        /// <param name="fileOperat"></param>
+        private void Operat(string source, Action<string> fileOperat)
+        {
+            if (Directory.Exists(source))
+            {
+                var subDirs = Directory.GetDirectories(source);
+                foreach (var d in subDirs)
+                {
+                    this.Operat(d, fileOperat);
+                }
+                var files = Directory.GetFiles(source);
+                foreach (var f in files)
+                {
+                    fileOperat(f);
+                }
+                return;
+            }
+
+            fileOperat(source);
+        }
+
+        /// <summary>
+        /// 首字母大写
+        /// </summary>
+        /// <param name="source"></param>
+        public void InitialUpper(string source)
+        {
+            this.Operat(source, (s) =>
+            {
+                var name = Path.GetFileNameWithoutExtension(s);
+                var ext = Path.GetExtension(s);
+                var targetName = name.Substring(0, 1).ToUpper() + name.Substring(1);
+                var targetFullName = targetName + ext;
+
+                this.FileRename(s, targetFullName);
+            });
+        }
+
+        /// <summary>
+        /// 首字母小写
+        /// </summary>
+        /// <param name="source"></param>
+        public void InitialLower(string source)
+        {
+            this.Operat(source, (s) =>
+            {
+                var name = Path.GetFileNameWithoutExtension(s);
+                var ext = Path.GetExtension(s);
+                var targetName = name.Substring(0, 1).ToLower() + name.Substring(1);
+                var targetFullName = targetName + ext;
+
+                this.FileRename(s, targetFullName);
+            });
+        }
+        /// <summary>
+        /// 全大写
+        /// </summary>
+        /// <param name="source"></param>
+        public void FullUpper(string source)
+        {
+            this.Operat(source, (s) =>
+            {
+                var name = Path.GetFileNameWithoutExtension(s);
+                var ext = Path.GetExtension(s);
+                var targetName = name.ToUpper();
+                var targetFullName = targetName + ext;
+
+                this.FileRename(s, targetFullName);
+            });
+        }
+        /// <summary>
+        /// 全小写
+        /// </summary>
+        /// <param name="source"></param>
+        public void FullLower(string source)
+        {
+            this.Operat(source, (s) =>
+            {
+                var name = Path.GetFileNameWithoutExtension(s);
+                var ext = Path.GetExtension(s);
+                var targetName = name.ToLower();
+                var targetFullName = targetName + ext;
+
+                this.FileRename(s, targetFullName);
+            });
+        }
+    }
+}
